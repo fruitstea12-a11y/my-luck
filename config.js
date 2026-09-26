26e50fb3d6b2f968cfc23f268dc13d05511671c5
fruitstea12-a11y <fruitstea12@gmail.com>
2026-09-26 10:30:40 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -12,15 +12,15 @@ window.LUCK_CONFIG = {
 // 비어 있는 칸은 화면에 나오지 않는다.
 // 전자상거래법상 통신판매 사이트에는 아래 항목을 표시해야 한다.
 window.LUCK_BUSINESS = {
-  name:     '',   // 상호            예) 내운사용설명서
-  owner:    '',   // 대표자
-  bizNo:    '',   // 사업자등록번호   예) 123-45-67890
-  mailNo:   '',   // 통신판매업 신고번호. 간이과세자로 면제면 '신고 면제(간이과세자)'
-  address:  '',   // 사업장 주소
-  phone:    '',   // 전화
+  name:     '내운사용설명서',   // 상호
+  owner:    '윤미령',   // 대표자
+  bizNo:    '160-34-01645',   // 사업자등록번호
+  mailNo:   '신고 면제(간이과세자)',   // 통신판매업 신고번호
+  address:  '서울특별시 노원구 덕릉로 595, 102동 1602호(중계동, 신안아파트)',   // 사업장 주소
+  phone:    '0502-1935-0777',   // 전화
   email:    'help@mywoon.kr',   // 문의 이메일
   kakao:    'https://pf.kakao.com/_MMBaX/chat',   // 카카오톡 채널 1:1 채팅 주소
-  privacyOfficer: '',   // 개인정보 보호책임자 (보통 대표자)
+  privacyOfficer: '윤미령',   // 개인정보 보호책임자 (보통 대표자)
   hosting:  'GitHub, Inc. (GitHub Pages)',   // 호스팅 서비스 제공자
   effective:'2026년 10월 1일'                // 정책 시행일
 };
